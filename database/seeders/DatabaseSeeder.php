b813dd56c5e567087382ded3f574d117db09fa5f
Teuku Fuad Maulana <fuad.pcd@gmail.com>
2024-02-20 17:07:03 +0700

    question crud

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -26,7 +26,8 @@ class DatabaseSeeder extends Seeder
             JurusanPnlSeeder::class,
             CriteriaSeeder::class,
             CriteriaJurusanSeeder::class,
-            QuestionTitleSeeder::class
+            QuestionTitleSeeder::class,
+            QuestionSeeder::class
         ]);
     }
 }
