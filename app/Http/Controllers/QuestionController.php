c4e5cbbaced83b18624e2301a7103fd10cbb0421
Teuku Fuad Maulana <fuad.pcd@gmail.com>
2024-02-20 17:06:39 +0700

    question crud

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -2,16 +2,74 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\QuestionRequest;
 use App\Http\Traits\RespondFormatter;
 use App\Models\Question;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class QuestionController extends Controller
 {
     use RespondFormatter;
 
-    public function index(Request $request)
+    public function show(Request $request,$questionTitleId)
     {
-        return $this->success('list soal',Question::with('choices')->get());
+        $data = Question::with('choices')
+            ->whereQuestionTitleId($questionTitleId)
+            ->get();
+        return $this->success('list soal',$data);
+    }
+
+    public function store(QuestionRequest $request)
+    {
+        try {
+            DB::beginTransaction();
+            $data =$request->validated();
+            $question = Question::create($data);
+
+            $question->choices()->createMany($data['choices']);
+            DB::commit();
+        } catch (\Exception $err)
+        {
+            DB::rollBack();
+            return $this->error($err->getMessage());
+        }
+
+        return $this->success('berhasil membuat soal',null);
+    }
+
+    public function update(QuestionRequest $request,$id)
+    {
+        $question = Question::findOrFail($id);
+
+        try {
+            DB::beginTransaction();
+            $data =$request->validated();
+
+           // hapus semua pilihan ganda
+            $question->choices()->delete();
+
+            // update data terbaru
+            $question->update($data);
+            $question->choices()->createMany($data['choices']);
+            DB::commit();
+        } catch (\Exception $err)
+        {
+            DB::rollBack();
+            return $this->error($err->getMessage());
+        }
+
+        return $this->success('berhasil memperbarui soal',null);
+    }
+
+    public function destroy($id)
+    {
+        $question = Question::findOrFail($id);
+
+        $question->choices()->delete();
+        $question->delete();
+
+
+        return $this->success('berhasil mengahpus soal',null);
     }
 }
---
+++ app/Http/Requests/QuestionRequest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Http\Traits\ErrorForm;
+use Illuminate\Foundation\Http\FormRequest;
+
+class QuestionRequest extends FormRequest
+{
+    use ErrorForm;
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => ['required','string'],
+            'question_title_id' => ['required','integer','exists:question_titles,id'],
+            'type' => ['required','boolean'],
+            'choices' => ['required','array'],
+            'choices.*.name' => ['required','string'],
+            'choices.*.point' => ['required','integer'],
+            'choices.*.type' => ['required','boolean'],
+        ];
+    }
+}
