407b27d3e39ab4fb389148e39f94116f69bb3a90
Teuku Fuad Maulana <fuad.pcd@gmail.com>
2024-05-20 16:02:43 +0700

    matikan seeder yang tidak perlu

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,7 +24,7 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => 'admin',
+            'nama' => 'admin',
             'email' => 'admin@gmail.com',
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
