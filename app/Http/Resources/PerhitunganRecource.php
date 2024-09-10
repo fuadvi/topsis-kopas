9f0db2374dec5f556c6d49396045badf3d924fbd
Teuku Fuad Maulana <fuad.pcd@gmail.com>
2024-09-10 15:33:31 +0700

    fix: perbaiki calculasi perhitungan metode

--- app/Http/Resources/PerhitunganRecource.php
+++ app/Http/Resources/PerhitunganRecource.php
@@ -14,11 +14,23 @@ class PerhitunganRecource extends JsonResource
      */
     public function toArray(Request $request) : array
     {
+        $calculation = json_decode($this?->calculation?? '') ?? [];
+
+        if ( $this?->position == 10 and !empty($calculation))
+        {
+            $calculation =  collect($calculation)->map(fn($perhitungan) => (object)[
+                "jurusan" => $perhitungan->jurusan,
+                "score" => $perhitungan->score / 3,
+            ]);
+        }
+
         return [
             "position" => $this?->position ?? '',
             "description" => $this?->description ?? '',
-            "calculation" =>  json_decode($this?->calculation?? '') ?? [],
+            "calculation" =>  $calculation ?? [],
             "metode" => $this?->metode ?? '',
         ];
     }
 }
+
+
